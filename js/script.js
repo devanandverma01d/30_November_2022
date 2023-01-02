560e615781034e2d398dc332f7ca85dd97de4aaf
Devanand Verma <devanandverma01d@gmail.come>
2023-01-02 15:17:15 +0530

    new

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*
-This area third where we can write JS code.
-*/
-console.log("Hello from external file");
-
-var aa = 1;
-var bb = 10;
---
+++ js/script1.js
@@ -0,0 +1,19 @@
+console.log("Hello from external file");
+var aa = 1;
+var bb = 10;
+var e = 15;
+var d = ++e;
+console.log("E = ", e, "and D = ", d);
+var x = 5;
+var y = x++
+console.log("X = ", x, "and Y = ", y);
+
+
+var p = 1;
+p = p + 1;
+p += 1
+
+var f = 10;
+f -= 1
+console.log("The value of P is: ", p);
+console.log("The value of f is: ", f)
