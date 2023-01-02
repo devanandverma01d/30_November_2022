f2f0693e0229edaffa45a817077375cf4e6ef51a
Devanand Verma <devanandverma01d@gmail.come>
2023-01-02 11:03:03 +0530

    new

---
+++ js/node.js
@@ -0,0 +1,12 @@
+//1. Variable declaration
+var x;
+
+//2. Variable initialisation
+var y;
+y = 15;
+
+
+//3. Variable declaration and initialisation
+var x = 50;
+
+console.log(x + y);
---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+This area third where we can write JS code.
+*/
+console.log("Hello from external file");
+
+var aa = 1;
+var bb = 10;
